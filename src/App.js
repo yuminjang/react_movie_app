9ac9ccd72a67d13bd4fd83973971de9c2c258345
DESKTOP-MJLHEHA\yumin <jym6158@kukudocs.com>
2023-09-24 22:14:38 +0900

    무비 리스트 만들기 시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading:false});
+    },3000);
+    
+  }
+  render(){
+    const {isLoading} = this.state;
+    return (
+      <div>{isLoading? 'loading..':'we are ready'}</div>
+    );
+  }
 }
 
 export default App;
